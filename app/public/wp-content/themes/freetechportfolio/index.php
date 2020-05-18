8c8f18644a588309189a4d0ca321326da111c193
freetechlearning <freetechlearning@gmail.com>
2020-05-18 01:49:08 -0300

    Aula 7 – O Loop

--- app/public/wp-content/themes/freetechportfolio/index.php
+++ app/public/wp-content/themes/freetechportfolio/index.php
@@ -6,5 +6,18 @@
  * @subpackage freetechportfolio
  */
  get_header();
+
+ if(have_posts()) :
+    while (have_posts()) : the_post();
+        the_title();
+        the_content();
+        the_author();
+    endwhile;
+    else :
+        _e('Não há posts para exibir', 'freetechportfolio');
+ endif;
+
+
+
  get_footer();
 ?>
